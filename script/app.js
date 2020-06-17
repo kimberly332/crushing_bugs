474b8ce7c2d0d2aab5f9daf075fc603f209522f9
kimberly332 <kimberly.wang1010@gmail.com>
2020-06-17 02:19:01 -0400

    added matching background to gameboard

--- script/app.js
+++ script/app.js
@@ -1,12 +1,18 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+				// querySelectorAll is for a one to many relationship and returns a Nodelust (an array) of matching elements
+				gameBoard = document.querySelector(".puzzle-board");
+				// one to one relationship -> returns the first matching element 
+
+
 
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
-		// and set the drop zone background
 
-		debugger;
+		// and set the drop zone background
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
+		// #gameBoard { background-image: url('../images/newBackground0.jpg')}
 	}
 
 	// add event handling here -> how is the user going to use our app?
@@ -14,4 +20,7 @@
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	//research call, apply and bind
+	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
 })();
